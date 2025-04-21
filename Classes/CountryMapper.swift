f973d612022a4142416a181bec307309d6b4590b
Amine <amine@allergomat.se>
2025-04-21 10:51:07 +0200

    AddedS endable

--- Classes/CountryMapper.swift
+++ Classes/CountryMapper.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 /// Helper enum for mapping country names to ISO codes
-public enum CountryCode: String, CaseIterable {
+public enum CountryCode: String, CaseIterable, Sendable {
     // European countries
     case AD = "AD"  // Andorra
     case AL = "AL"  // Albania
