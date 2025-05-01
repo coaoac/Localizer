e71afb1a7cfb4451b99cd0a87a61b9e6fad24913
Amine <admin@allergomat.se>
2025-05-01 11:12:10 +0200

    made public

--- Classes/Data/DataExtensions.swift
+++ Classes/Data/DataExtensions.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-extension Data {
+public extension Data {
     // --- Nested Enums for Type and Encoding ---
     public enum Format: String, Codable, Sendable, Equatable, Hashable {
         case jpeg = "image/jpeg"
